0b3dd641e4b859b4ebcc007719cbab998857b82c
Альберт Олег Витальевич <oleg.albert.99@mail.ru>
2023-07-18 08:18:40 +0300

    Добавлен класс Cat, продемонстрирована работа с аннотациями Spring

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,14 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        //System.out.println(bean1.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1 == bean2);
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        //System.out.println(cat1.getName());
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat1 == cat2);
     }
 }
